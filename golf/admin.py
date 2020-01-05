f449c545e7bad2a97acf19c7bf72f86dbb092a64
pincoin <pincoins@gmail.com>
2020-01-05 11:09:16 +0700

    - holidays

--- golf/admin.py
+++ golf/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from . import models
+
+admin.site.site_header = _('NA-ONE Communication')
+admin.site.site_title = _('NA-ONE Communication')
+admin.site.index_title = _('NA-ONE Communication')
+
+
+class HolidayAdmin(admin.ModelAdmin):
+    list_display = ('holiday', 'country', 'title')
+    list_filter = ('holiday', 'country')
+    search_fields = ('title',)
+    ordering = ('-holiday',)
+    date_hierarchy = 'holiday'
+
+
+admin.site.register(models.Holiday, HolidayAdmin)
