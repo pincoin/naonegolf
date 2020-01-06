ce8613969ca67e6333d6474a6077135db5e54759
pincoin <pincoins@gmail.com>
2020-01-06 23:27:57 +0700

    - base template, i18n

--- conf/urls.py
+++ conf/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import (
+    path, include
+)
 
 from .views import HomeView
 
 urlpatterns = [
     path('',
          HomeView.as_view(), name='home'),
+
+    path('accounts/',
+         include('allauth.urls')),
 
     path('admin/',
          admin.site.urls),
