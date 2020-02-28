bf3f8a9ef3c3880ee466b143c7e276f8a69f46fd
pincoin <pincoins@gmail.com>
2020-02-28 21:04:30 +0700

    - no tables

--- windmill/forms.py
+++ windmill/forms.py
@@ -2,18 +2,3 @@ from django import forms
 
 from . import models
 
-
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = models.Booking
-        fields = [
-            'round_time', 'pax', 'hole',
-            'green_fee_sales', 'caddie_fee_sales', 'cart_fee_sales',
-            'green_fee_pay_on_arrival', 'caddie_fee_pay_on_arrival', 'cart_fee_pay_on_arrival',
-            'green_fee_cost', 'caddie_fee_cost', 'cart_fee_cost', 'cart_fee_deducted_from_deposit',
-            'first_name', 'last_name', 'memo',
-            'season', 'day_of_week', 'slot', 'stand_by',
-        ]
-
-    def clean(self):
-        pass
--- windmill/views.py
+++ windmill/views.py
@@ -10,15 +10,6 @@ class TeeOffListView(generic.ListView):
 
     template_name = 'windmill/tee_off_list.html'
 
-    def get_queryset(self):
-        queryset = models.DailyBooking.objects \
-            .prefetch_related('teeofftime_set') \
-            .filter(day__gte=timezone.make_aware(timezone.localtime().today())) \
-            .order_by('day')
-
-        return queryset
-
-
 class BookingCreateForm(generic.CreateView):
-    form_class = forms.BookingForm
+
     template_name = 'windmill/booking_create.html'
