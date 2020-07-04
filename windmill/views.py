58c0b0e12e26f06fc116e8c1af692d8a8b7e0da2
pincoin <pincoins@gmail.com>
2020-07-05 02:57:15 +0900

    - total profit

--- windmill/views.py
+++ windmill/views.py
@@ -20,8 +20,12 @@ class DailyStatusReport(generic.ListView):
     context_object_name = 'tee_off_times'
     template_name = 'windmill/daily_status_report.html'
 
+    total_sales = 0
+    total_cost = 0
+    total_profit = 0
+
     def get_queryset(self):
-        return models.TeeOffTime.objects \
+        qs = models.TeeOffTime.objects \
             .select_related('agency') \
             .prefetch_related('naoneassettransaction_set__asset') \
             .filter(day__year=self.kwargs['year'],
@@ -39,4 +43,30 @@ class DailyStatusReport(generic.ListView):
                                                         naoneassettransaction__asset__asset_type
                                                         =models.NaoneAsset.ASSET_TYPE_CHOICES.petty_cash),
                                                ),
+                      total_sales=Sum('naoneassettransaction__amount',
+                                      filter=Q(naoneassettransaction__cash_flow
+                                               =models.NaoneAssetTransaction.CASH_FLOW_CHOICES.cash_in),
+                                      ),
+                      total_cost=Sum('naoneassettransaction__amount',
+                                     filter=Q(naoneassettransaction__cash_flow
+                                              =models.NaoneAssetTransaction.CASH_FLOW_CHOICES.cash_out),
+                                     ),
                       )
+
+        for teeoff in qs:
+            for transaction in teeoff.naoneassettransaction_set.all():
+                if transaction.cash_flow == models.NaoneAssetTransaction.CASH_FLOW_CHOICES.cash_in:
+                    self.total_sales += transaction.amount
+                elif transaction.cash_flow == models.NaoneAssetTransaction.CASH_FLOW_CHOICES.cash_out:
+                    self.total_cost += transaction.amount
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(DailyStatusReport, self).get_context_data(**kwargs)
+
+        context['total_sales'] = self.total_sales
+        context['total_cost'] = self.total_cost
+        context['total_profit'] = self.total_sales - self.total_cost
+
+        return context
